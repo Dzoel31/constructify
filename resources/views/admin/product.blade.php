6f1d4a0a0c338da3b1f88956ca77cabd36987c10
Dzoel31 <dzulfikriadjmal@gmail.com>
2024-06-21 09:27:14 +0700

    ref: fix typo

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -45,7 +45,7 @@
 
                         <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                             <h3 class="text-lg font-semibold text-gray-900 dark:text-black">
-                                Update Product
+                                Add Product
                             </h3>
                             <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-black" data-modal-toggle="add-modal">
                                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -102,7 +102,7 @@
                                 </div>
                             </div>
                             <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Save
+                                Add
                             </button>
                         </form>
                     </div>
